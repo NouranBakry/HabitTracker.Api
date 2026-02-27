443d1bc604b13946b7405babb7b2a2e54c9d4cbe
NouranBakry <nouranaymanbakry@gmail.com>
2026-02-27 02:29:29 +0200

    fixing create habit entry

--- HabitTracker.Application/Interfaces/IHabitEntryRepository.cs
+++ HabitTracker.Application/Interfaces/IHabitEntryRepository.cs
@@ -2,7 +2,7 @@ using HabitTracker.Domain.Entities;
 
 public interface IHabitEntryRepository
 {
-    Task<HabitEntry> GetByIdAsync(Guid id);
+    Task<HabitEntry?> GetByIdAsync(Guid id);
     Task AddAsync(HabitEntry entry);
     Task UpdateAsync(HabitEntry entry);
     Task DeleteAsync(Guid id);
--- HabitTracker.Infrastructure/AppDbContextFactory.cs
+++ HabitTracker.Infrastructure/AppDbContextFactory.cs
@@ -1,27 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
-using System.IO;
 
 namespace HabitTracker.Infrastructure;
 
+// This class tells EF exactly how to create the DbContext during migrations
 public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
 {
     public AppDbContext CreateDbContext(string[] args)
     {
-        // 1. Get the path to the API project where appsettings.json lives
-        IConfigurationRoot configuration = new ConfigurationBuilder()
-            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../HabitTracker.Api"))
-            .AddJsonFile("appsettings.json")
-            .Build();
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
 
-        var builder = new DbContextOptionsBuilder<AppDbContext>();
+        // HARDCODE your dev string here temporarily just to get the DB built.
+        // This is a common "Senior" trick to bypass configuration loading issues during local setup.
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=HabitTrackerDb;Username=postgres;Password=postgres");
 
-        // 2. Use the exact key from your appsettings.json
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
-
-        builder.UseNpgsql(connectionString);
-
-        return new AppDbContext(builder.Options);
+        return new AppDbContext(optionsBuilder.Options);
     }
 }
